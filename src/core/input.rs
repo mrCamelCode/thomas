cc304ed459a1520ff92ffa6e4058ece284279981
jt <unluckycricketgames@gmail.com>
2023-03-25 18:09:56 -0400

    wrote input service

--- src/core/input.rs
+++ src/core/input.rs
@@ -1,38 +1,105 @@
-use std::{
-    collections::HashMap,
-    io,
-    sync::mpsc::{self, Receiver},
-    thread,
-};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std::collections::HashMap;
 
-// TODO: The crate device_query should enable this service's existence.
+#[derive(Clone, PartialEq)]
+enum KeyState {
+    Up,
+    Down,
+}
+
+#[derive(Clone)]
+struct KeyStateData {
+    prev_state: KeyState,
+    current_state: KeyState,
+}
+
 pub struct Input {
-    keylogger: HashMap<String, bool>
+    keylogger: HashMap<Keycode, KeyStateData>,
+    device_state: DeviceState,
 }
 
 impl Input {
     pub fn new() -> Self {
         Input {
             keylogger: HashMap::new(),
+            device_state: DeviceState::new(),
         }
     }
 
     /// Whether the key was pressed down this frame.
-    pub fn is_key_down(&self) -> bool {
-        todo!();
+    pub fn is_key_down(&self, keycode: &Keycode) -> bool {
+        if let Some(key_state_data) = self.keylogger.get(keycode) {
+            return key_state_data.current_state == KeyState::Down
+                && key_state_data.prev_state == KeyState::Up;
+        }
+
+        false
     }
 
-    /// Whether the key was let up this frame.
-    pub fn is_key_up(&self) -> bool {
-        todo!();
+    /// Whether the key was released this frame.
+    pub fn is_key_up(&self, keycode: &Keycode) -> bool {
+        if let Some(key_state_data) = self.keylogger.get(keycode) {
+            return key_state_data.current_state == KeyState::Up
+                && key_state_data.prev_state == KeyState::Down;
+        }
+
+        false
     }
 
-    /// Whether the key was pressed this frame.
-    pub fn is_key_pressed(&self) -> bool {
-        todo!();
+    /// Whether the key is pressed. This will return `true` on every frame while the key is pressed
+    /// down. If you want to do something on the one frame in which the key was pressed down, use `is_key_down`.
+    pub fn is_key_pressed(&self, keycode: &Keycode) -> bool {
+        if let Some(key_state_data) = self.keylogger.get(keycode) {
+            return key_state_data.current_state == KeyState::Down;
+        }
+
+        false
     }
 
     pub(crate) fn update(&mut self) {
-        todo!();
+        let current_keys = self.device_state.get_keys();
+
+        self.keylogger.iter_mut().for_each(|(_, key_state_data)| {
+            key_state_data.prev_state = key_state_data.current_state.clone();
+            key_state_data.current_state = KeyState::Up;
+        });
+
+        current_keys.iter().for_each(|keycode| {
+            if let Some(key_state_data) = self.keylogger.get(keycode) {
+                let mut new_key_state_data = key_state_data.clone();
+
+                new_key_state_data.current_state = KeyState::Down;
+
+                self.keylogger.insert(keycode.clone(), new_key_state_data);
+            } else {
+                self.keylogger.insert(
+                    keycode.clone(),
+                    KeyStateData {
+                        prev_state: KeyState::Up,
+                        current_state: KeyState::Down,
+                    },
+                );
+            }
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod key_state {
+        use super::*;
+
+        #[test]
+        fn equality_works() {
+            assert!(KeyState::Up == KeyState::Up);
+            assert!(KeyState::Down == KeyState::Down);
+        }
+
+        #[test]
+        fn inequality_works() {
+            assert!(KeyState::Up != KeyState::Down);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod core;
 
 pub use thomas_derive::*;
+
+pub use device_query::Keycode;
